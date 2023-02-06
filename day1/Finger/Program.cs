b5ebd0dc115c704aa618961fc2658f75a9b5ad01
Joel Maus <joel_maus@progressive.com>
2023-02-06 15:21:56 -0500

    changes

--- day1/Finger/Program.cs
+++ day1/Finger/Program.cs
@@ -1,13 +1,32 @@
-﻿namespace Finger
+﻿using System;
+using Utils;
+
+namespace Finger
 {
+    
     public class Program
     {
         //Entry point to application
         public static void Main()
         {
-            System.Console.WriteLine("Hello, World!");
+                Console.WriteLine("What is your status?");
+                string? status = Console.ReadLine();
+
+            if (status != null)
+            {
+            StatusMessage myStatus = new StatusMessage("I'm good", DateTimeOffset.Now);
+                Console.WriteLine($"you said your status was {myStatus.Status} at {myStatus.When:T}");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, cannot have a null status");
+            }
         }
+
     }
+}
 
-
+namespace Utils
+{
+    public record StatusMessage(string Status, DateTimeOffset When);
 }
