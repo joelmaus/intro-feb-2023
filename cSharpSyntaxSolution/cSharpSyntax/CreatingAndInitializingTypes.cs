327506ac5dcd1969f603de483b478c71cabdb81d
Joel Maus <joel_maus@progressive.com>
2023-02-07 15:31:30 -0500

    timer for break

--- cSharpSyntaxSolution/cSharpSyntax/CreatingAndInitializingTypes.cs
+++ cSharpSyntaxSolution/cSharpSyntax/CreatingAndInitializingTypes.cs
@@ -64,6 +64,46 @@ namespace cSharpSyntax
 
             Assert.Equal(message, "Old Enough");
         }
+        [Fact]
+        public void MutableStringsWithStringBuilders()
+        {
+            var message = new StringBuilder();
+
+            foreach(var num in Enumerable.Range(1,10000))
+            {
+                message.Append(num.ToString() + ", ");
+            }
+            Assert.True(message.ToString().StartsWith("1, 2, 3, 4"));
+        }
+
+
+        [Fact]
+        public void MoreAboutStrings()
+        {
+            var name = "Bob";
+            var age = 33;
+
+            var message = name + " is " + age;
+            var message2 = string.Format("{0} is {1}", name, age);
+            var message3 = $"{name} is {age}";
+        }
+        [Fact]
+        public void DoingConversionsOnTypes()
+        {
+            string myPay = "100.83Tacos";
+
+            if (decimal.TryParse(myPay, out decimal payAsNumber))
+            {
+                Assert.Equal(10_000.83M, payAsNumber);
+            }
+            else
+                Assert.True(false);
+
+            var birthdate = DateTime.Parse("09/10/1987"); 
+
+        }
+
+
     }
 
     public class Taco { }
