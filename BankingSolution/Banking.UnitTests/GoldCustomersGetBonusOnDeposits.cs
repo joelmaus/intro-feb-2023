8428345f80d8885915b1fcc84345d3c448cf9a80
Joel Maus <joel_maus@progressive.com>
2023-02-09 11:05:16 -0500

    gold customer bonus

---
+++ BankingSolution/Banking.UnitTests/GoldCustomersGetBonusOnDeposits.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.UnitTests
+{
+   public class GoldCustomersGetBonusOnDeposits
+    {
+
+        [Fact]
+        public void BonusAppliedToDeposit()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var amountToDeposit = 100M;
+
+            account.Deposit(amountToDeposit);
+
+            Assert.Equal(openingBalance + amountToDeposit + 10M, account.GetBalance());
+        }
+    }
+}
