1226913f21d9fb2ca8b2dba8a8d20589a152526c
Joel Maus <joel_maus@progressive.com>
2023-02-09 16:23:56 -0500

    all passing tests

---
+++ BankingSolution/Banking.Domain/SystemTime.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Banking.Domain
+{
+    public interface ISystemTime
+    {
+        DateTime GetCurrent();
+    }
+    public class SystemTime : ISystemTime
+    {
+        public DateTime GetCurrent()
+        {
+            return DateTime.Now;
+        }
+    }
+         
+}
