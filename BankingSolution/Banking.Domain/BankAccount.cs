6b828f13eafdd1e29aea26462ed1f8e181bc7c76
Joel Maus <joel_maus@progressive.com>
2023-02-09 12:24:14 -0500

    update

--- BankingSolution/Banking.Domain/BankAccount.cs
+++ BankingSolution/Banking.Domain/BankAccount.cs
@@ -4,6 +4,12 @@
     {
         private decimal _balance = 5000M;
         private ICanCalculateAccountBonuses _bonusCalculator;
+
+        public BankAccount(ICanCalculateAccountBonuses bonusCalculator)
+        {
+            _bonusCalculator = bonusCalculator;
+        }
+
         public virtual void Deposit(decimal amountToDeposit)
         {
             decimal bonus = _bonusCalculator.GetDepositBonusFor(_balance, amountToDeposit);
