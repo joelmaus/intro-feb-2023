3aab3ab4129be1079cf6f69ce76597d1a986a960
Joel Maus <joel_maus@progressive.com>
2023-02-10 15:49:32 -0500

    api started

---
+++ backend/LearningResourcesSolution/LearningResourcesApi.IntegrationTests/StatusResourceTests.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace LearningResourcesApi.IntegrationTests
+{
+    public class StatusResourceTests
+    {
+        [Fact]
+        public async Task TheStatusResource()
+        {
+             using var host = await AlbaHost.For<Program>();
+
+            await host.Scenario(api => //integration test (many steps)
+            {
+                api.Get.Url("/status");
+                api.StatusCodeShouldBeOk(); //200 status code
+            });
+        }
+    }
+}
---
+++ backend/LearningResourcesSolution/LearningResourcesApi/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningResourcesApi.Controllers
+{
+    public class StatusController : ControllerBase
+    {
+        //GET http:localhost:1337/status
+        [HttpGet("/status")]
+        public ActionResult GetTheStatus()
+        {
+            var response = new GetStatusResponse
+            {
+                Message = "All Good",
+                Contact = "555 555-5555"
+            };
+            return Ok(response); //sends "response" to client
+        }
+    }
+}
+
+public class GetStatusResponse
+{
+    public string Message { get; set; } = string.Empty;
+    public string Contact { get; set; } = string.Empty;
+}
