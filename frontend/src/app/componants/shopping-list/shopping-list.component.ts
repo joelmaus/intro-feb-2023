f601cfdbea75e968f1d5b577492c25be8a5fde04
Joel Maus <joel_maus@progressive.com>
2023-02-13 15:37:10 -0500

    updated

--- frontend/src/app/componants/shopping-list/shopping-list.component.ts
+++ frontend/src/app/componants/shopping-list/shopping-list.component.ts
@@ -1,30 +1,50 @@
 import { Component } from '@angular/core';
-import {FormControl, FormGroup } from '@angular/forms';
-import { ConsoleReporter } from 'jasmine';
- @Component({
-    selector: 'app-shopping-list',
-    templateUrl: './shopping-list.component.html',
-    styleUrls: ['./shopping-list.component.css']
-  })
-  export class ShoppingListComponent {   
+import { FormControl, FormGroup } from '@angular/forms';
 
-    form = new FormGroup({
-      item: new FormControl('beer')
-    });
-    
-    items: ShoppingItem[] = [
-      { description: 'Beer', purchased: false},
-      { description: 'Buns', purchased: false},
-      { description: 'Eggs', purchased: true}
-    ];
-markPurchased(item:ShoppingItem){
-  item.purchased=true;
-}
-addItem(){
-  console.log(this.form.value);
-}
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent {
 
- } type ShoppingItem = {
-    description:string,
-    purchased: boolean
+  form = new FormGroup({
+    item: new FormControl<string>('', { nonNullable: true})
+  });
+  // state - data
+  items: ShoppingItem[] = [
+    { description: 'Beer', purchased: false},
+    { description: 'Buns', purchased: false},
+    { description: 'Eggs', purchased: true}
+  ];
+
+
+  clearPurchased() {
+    this.items = this.items.filter(item => item.purchased === false);
   }
+
+  get hasPurchasedItems() {
+    return this.items.filter(item => item.purchased).length > 0;
+  }
+  // behavior - methods
+  markPurchased(item:ShoppingItem) {
+    item.purchased = true;
+  }
+
+  addItem() {
+   
+    const description = this.form.controls.item.value;
+    const newItem:ShoppingItem = {
+      description: description,
+      purchased: false
+    }
+
+    this.items = [newItem, ...this.items];
+
+  }
+}
+
+type ShoppingItem = {
+  description:string,
+  purchased: boolean
+}
