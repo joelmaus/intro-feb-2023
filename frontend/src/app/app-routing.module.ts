c6278f1a73d246c270448a90e42c8cda0cd53bbd
Joel Maus <joel_maus@progressive.com>
2023-02-15 16:56:57 -0500

    added angular files

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'shopping',
+    component: ShoppingListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'counter',
+    component: CounterComponent
+  },
+  {
+    path: '**', // <-- Angular syntax that matches ANYTHING
+    redirectTo: 'dashboard'
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
