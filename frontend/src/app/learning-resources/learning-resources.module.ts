74b0b20f8ab5a788d94952dfad1fbe938a0128d1
Joel Maus <joel_maus@progressive.com>
2023-02-16 11:51:52 -0500

    added files

--- frontend/src/app/learning-resources/learning-resources.module.ts
+++ frontend/src/app/learning-resources/learning-resources.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LearningResourcesComponent } from './learning-resources.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NewComponent } from './components/new/new.component';
 
 
 const routes: Routes = [
@@ -13,7 +14,8 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    LearningResourcesComponent
+    LearningResourcesComponent,
+    NewComponent
   ],
   imports: [
     CommonModule,
