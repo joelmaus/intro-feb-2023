88ca02c379ebc7e6c079fb8597330ef0f8458960
Joel Maus <joel_maus@progressive.com>
2023-02-17 13:41:43 -0500

    updated

--- backend/LearningResourcesSolution/LearningResourcesApi/Controllers/ResourcesController.cs
+++ backend/LearningResourcesSolution/LearningResourcesApi/Controllers/ResourcesController.cs
@@ -1,4 +1,5 @@
 ﻿using LearningResourcesApi.Adapters;
+using LearningResourcesApi.Domain;
 using LearningResourcesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,25 @@ public class ResourcesController : ControllerBase
         _context = context;
     }
 
+    [HttpGet("/resources/{id:int}")]
+    public async Task<ActionResult> GetById(int id)
+    {
+
+        var response = await GetResourceItemQuery()
+            .Where(i => i.Id == id.ToString())
+            .ToListAsync();
+
+        if (response == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            return Ok(response);
+        }
+    }
+
+
     [HttpPost("/resources")]
     public async Task<ActionResult> AddItem([FromBody] CreateResourceItem request)
     {
@@ -23,31 +43,44 @@ public class ResourcesController : ControllerBase
         {
             return BadRequest(ModelState);
         }
-        // tomorrow - ADD IT TO THE DATABASE
-        var response = new GetResourceItem
+
+        var itemToSave = new LearningItem
         {
-            Id = Guid.NewGuid().ToString(),
             Description = request.Description,
             Link = request.Link,
             Type = request.Type,
         };
-        return Ok(response);
+
+        _context.Items.Add(itemToSave);
+        await _context.SaveChangesAsync();
+        var response = new GetResourceItem
+        {
+            Id = itemToSave.Id.ToString(),
+            Description = itemToSave.Description,
+            Link = itemToSave.Link,
+            Type = itemToSave.Type,
+        };
+        return StatusCode(201, response);
     }
 
     [HttpGet("/resources")]
     public async Task<ActionResult> GetResources()
     {
         // Mapping - copying from a to b =
-        var items = await _context.Items
-            .Select(item => new GetResourceItem
-            {
-                Id = item.Id.ToString(),
-                Description = item.Description,
-                Link = item.Link,
-                Type = item.Type
-            }).ToListAsync();
+        var items = await GetResourceItemQuery().ToListAsync();
 
         var response = new GetResourcesResponse { Items = items };
         return Ok(response);
+    }
+
+    private IQueryable<GetResourceItem> GetResourceItemQuery()
+    {
+        return _context.Items.Select(item => new GetResourceItem
+        {
+            Id = item.Id.ToString(),
+            Description = item.Description,
+            Link = item.Link,
+            Type = item.Type
+        });
     }
 }
